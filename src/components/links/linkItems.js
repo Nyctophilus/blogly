306d3cb779d2bd90be9941fb9dbbad05c10ca312
Medo <mohammed.yuossry@gmail.com>
2024-01-21 15:55:55 +0200

    style active links

--- src/components/links/linkItems.js
+++ src/components/links/linkItems.js
@@ -1,20 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./links.module.css";
+import { usePathname } from "next/navigation";
+import { signOutGit } from "@/lib/actions";
 
 const links = ["home", "about", "contact us", "blogs"];
 
-const LinkItems = ({ row }) => {
+const LinkItems = ({ row, session }) => {
   const horStyles = "md:flex gap-2 items-center hidden";
   const VerStyles =
     "flex flex-col gap-6 items-center justify-center gap-4 py-6 md:hidden rounded-2xl bg-[var(--bg-900)] fixed top-[80px] right-[0] h-[calc(100svh-120px)] w-[20svh] overflow-hidden z-10";
+  const linkStyle = `${
+    !row && styles.link
+  } capitalize w-full text-center hover:bg-[var(--bg-alt)] whitespace-nowrap py-1 px-2 rounded-lg`;
+
+  const pathname = usePathname();
 
   return (
     <div className={row ? horStyles : VerStyles}>
       {links.map((link) => (
         <Link
-          className={`${
-            !row && styles.link
-          } capitalize w-full text-center hover:bg-[var(--bg-alt)] whitespace-nowrap py-1 px-2 rounded-lg`}
+          className={`${linkStyle} ${
+            link === "home" && pathname === "/"
+              ? styles.active
+              : pathname === `/${link.split(" ")[0]}` && styles.active
+          }`}
           key={link}
           href={`/${link === "home" ? "" : link.split(" ")[0]}`}
         >
@@ -22,22 +33,26 @@ const LinkItems = ({ row }) => {
         </Link>
       ))}
 
-      <Link
-        className={`${
-          !row && styles.link
-        } capitalize w-full text-center hover:bg-[var(--bg-alt)] py-1 px-2 rounded-lg`}
-        href={"/admin"}
-      >
-        admin
-      </Link>
-      <Link
-        className={`${
-          !row && styles.link
-        } capitalize w-full text-center hover:bg-[var(--bg-alt)] py-1 px-2 rounded-lg`}
-        href={"/login"}
-      >
-        login
-      </Link>
+      {session?.user?.isAdmin && (
+        <Link className={`${linkStyle} ${styles.active}`} href={"/admin"}>
+          admin
+        </Link>
+      )}
+
+      {session?.user ? (
+        <form action={signOutGit}>
+          <button type="submit" className={`${linkStyle} ${styles.active}`}>
+            logout
+          </button>
+        </form>
+      ) : (
+        <Link
+          className={`${linkStyle} ${pathname === "/login" && styles.active}`}
+          href={"/login"}
+        >
+          login
+        </Link>
+      )}
     </div>
   );
 };
--- src/components/links/links.js
+++ src/components/links/links.js
@@ -1,10 +1,13 @@
 import Sidebar from "../sidebar/sidebar";
 import LinkItems from "./linkItems";
+import { auth } from "@/lib/auth";
 
-const Links = () => {
+const Links = async () => {
+  const session = await auth();
+
   return (
     <nav>
-      <LinkItems row />
+      <LinkItems row session={session} />
       <Sidebar />
     </nav>
   );
