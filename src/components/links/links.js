f09cb1ac99a9efd3815e7bb7550d466d9dd7b9a1
Medo <mohammed.yuossry@gmail.com>
2024-01-25 00:21:30 +0200

    fix: missing username on credentials login

--- src/components/links/links.js
+++ src/components/links/links.js
@@ -1,13 +1,15 @@
+import { getUser } from "@/lib/data";
 import Sidebar from "../sidebar/sidebar";
 import LinkItems from "./linkItems";
 import { auth } from "@/lib/auth";
 
 const Links = async () => {
   const session = await auth();
+  const { username } = await getUser(session.user?.id);
 
   return (
     <nav>
-      <LinkItems row session={session} />
+      <LinkItems row session={session} uName={username} />
       <Sidebar />
     </nav>
   );
