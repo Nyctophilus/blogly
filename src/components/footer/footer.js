085bb9d58cbbf7e0e3aa9b597fd498b07dc38c6c
Medo <mohammed.yuossry@gmail.com>
2024-02-05 02:38:09 +0200

    colored the footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="container text-center py-2 text-pretty">
-      &copy;2024 created and designed by Muhammed Yousry Fayad
+    <footer className="text-center py-4 w-full mt-6 bg-blue-gray-600/20 ">
+      <div className="container text-pretty">
+        &copy; 2024 created and designed by Muhammed Yousry Fayad
+      </div>
     </footer>
   );
 };
